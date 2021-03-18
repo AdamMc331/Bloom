2948a53230fea4a6ec36563aa493eabd84fd3aff
adammc331 <amcneilly331@gmail.com>
2021-03-17 22:21:31 -0400

    Adding bottom app bar.

--- app/src/main/java/com/adammcneilly/bloom/HomeScreen.kt
+++ app/src/main/java/com/adammcneilly/bloom/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,23 +14,91 @@ import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.adammcneilly.bloom.ui.theme.BloomTheme
 
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            BloomBottomBar()
+        }
+    ) {
+        HomeScreenContent()
+    }
+}
+
+@Composable
+private fun BloomBottomBar() {
+    BottomAppBar(
+        backgroundColor = MaterialTheme.colors.primary,
+    ) {
+        BloomBottomButton(
+            selected = true,
+            icon = Icons.Default.Home,
+            labelText = "Home"
+        )
+        BloomBottomButton(
+            selected = false,
+            icon = Icons.Default.FavoriteBorder,
+            labelText = "Favorites"
+        )
+        BloomBottomButton(
+            selected = false,
+            icon = Icons.Default.AccountCircle,
+            labelText = "Profile"
+        )
+        BloomBottomButton(
+            selected = false,
+            icon = Icons.Default.ShoppingCart,
+            labelText = "Cart"
+        )
+    }
+}
+
+@Composable
+private fun RowScope.BloomBottomButton(
+    selected: Boolean,
+    icon: ImageVector,
+    labelText: String,
+) {
+    BottomNavigationItem(
+        selected = selected,
+        onClick = { /*TODO*/ },
+        icon = {
+            Icon(
+                icon,
+                contentDescription = null,
+            )
+        },
+        label = {
+            Text(labelText)
+        }
+    )
+}
+
+@Composable
+private fun HomeScreenContent() {
     Surface(
         color = MaterialTheme.colors.background,
         modifier = Modifier
